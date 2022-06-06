8fc0a121b506be9e95c3a887f0ccabb7e6379dc4
mahsa-fotros <fotros.mahsa@gmail.com>
2022-06-06 16:35:49 +0430

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Mahsa Fotros and is{" "}
+        <a
+          href="https://github.com/mahsa-fotros/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
