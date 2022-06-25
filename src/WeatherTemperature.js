8e8d043ed5b5c5b5f3fe61c66058d2b14f61087b
mahsa-fotros <fotros.mahsa@gmail.com>
2022-06-25 16:48:43 +0430

    Simplify units

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,41 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsuis");
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  function showCelsuis(event) {
-    event.preventDefault();
-    setUnit("celsuis");
-  }
-  function fahrenheit() {
-    return props.celsuis * (9 / 5) + 32;
-  }
-  if (unit === "celsuis") {
-    return (
-      <div className="WeatherTemperature">
-        <strong id="temperature">{props.celsuis}</strong>
-        <span className="degree-symbol">
-          ℃ |{" "}
-          <a href="/" onClick={showFahrenheit}>
-            ℉
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherTemperature">
-        <strong id="temperature">{Math.round(fahrenheit())}</strong>
-        <span className="degree-symbol">
-          <a href="/" onClick={showCelsuis}>
-            ℃
-          </a>{" "}
-          | ℉
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <strong id="temperature">{props.celsuis}</strong>
+      <span className="degree-symbol">℃</span>
+    </div>
+  );
 }
